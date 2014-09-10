fb7d6d0ff997de4d187899bbd16890a7336eb066
Vladimir Meremyanin <vladimir@meremyanin.com>
2014-09-10 09:21:14 +0400

    more specs for event callbacks

--- test/test_event_guard.rb
+++ test/test_event_guard.rb
@@ -8,12 +8,20 @@ class TestEventGuard < Minitest::Test
       state :constructed, :initial => true
       state :broken
 
-      # guards can be inline proc, or symbols
-      event :break, :from => :constructed, :to => :broken, :guard => proc { stones < 5 }
+      event :break do
+        # guards can be inline proc, or symbols
+        from :constructed, :to => :broken, :guard => proc { stones < 5 }
 
-      # event parameters are passed to guards only if arity > 0
-      event :break, :from => :constructed, :to => :broken, :guard => :breakable?
+        # event parameters are passed to guards only if arity > 0
+        from :constructed, :to => :broken, :guard => :breakable?
+
+        before do |strength|
+          @last_hit_strength = strength.to_s
+        end
+      end
     end
+
+    attr_reader :last_hit_strength
 
     def breakable?(hits = 1)
       hits * 5 > stones
@@ -24,6 +32,8 @@ class TestEventGuard < Minitest::Test
     wall = Wall.new(3)
     wall.break
     assert_equal 'broken', wall.state
+    # before callback invoked
+    assert_equal '', wall.last_hit_strength
   end
 
   def test_cant_break_wall_10_stones_thick
@@ -31,12 +41,16 @@ class TestEventGuard < Minitest::Test
     assert_raises(Hifsm::MissingTransition) do
       wall.break
     end
+    # before callback not called
+    assert_equal nil, wall.last_hit_strength
   end
 
   def test_can_break_thick_wall_if_hit_3_times
     wall = Wall.new(10)
     wall.break 3
     assert_equal 'broken', wall.state
+    # before callback invoked
+    assert_equal '3', wall.last_hit_strength
   end
 
 end
